d94665609c95fd83e372a133177bee5c028070e7
Gerten Pilv <50708122221@tptlive.ee>
2024-11-12 14:11:59 +0200

    add greetings

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
     </div>
   )
 }
 
 const App = () => {
+  const name = "Aleks"
+  const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name="Kaupo" age={26 + 10}/>
+      <Hello name={name} age={age}/>
     </div>
   )
 }
