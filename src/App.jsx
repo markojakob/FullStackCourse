9d41ac18f0723f2b723811b3c6a3d6a98da75ba3
GertenP <gerten.pilv@tptlive.ee>
2024-11-12 14:26:53 +0200

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ const Hello = (props) => {
 
 //git
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/GertenP">Gerten Pilv</a>
+    </div>
+  )
+}
+
 const App = () => {
   const name = "Aleks"
   const age = 10
@@ -17,6 +25,7 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name="Kaupo" age={26 + 10}/>
       <Hello name={name} age={age}/>
+      <Footer />
     </div>
   )
 }
