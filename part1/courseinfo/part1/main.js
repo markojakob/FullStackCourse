fb31dbfe9262e39139f164ade5439bd40552f2d2
GertenP <gerten.pilv@tptlive.ee>
2024-11-19 13:22:15 +0200

    ex 1.5

--- part1/courseinfo/part1/main.js
+++ part1/courseinfo/part1/main.js
@@ -1,5 +1,16 @@
-let t = [1, 2, 3]
-let t2 = []
-t.map(element => {console.log("aleks")})
+class Person {
+    constructor(name,age){
+        this.name = name
+        this.age = age
+    }
 
-t.forEach(element => {console.log(element)});
+    greet(){
+        console.log("Hello minu nimi on " + this.name + " ja olen " + this.age + " vana")
+    }
+}
+
+const kelly = new Person("Kelly", 18)
+kelly.greet()
+
+const aleks = new Person("Aleks", 18)
+aleks.greet()
--- part1/courseinfo/part1/src/App.jsx
+++ part1/courseinfo/part1/src/App.jsx
@@ -1,38 +1,35 @@
 const Header = (props) => {
-  console.log(props)
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
+  return <h1>{props.course.name}</h1>;
+};
 
 const Content = (props) => {
-  return (
-    <p>{props.part} {props.exercise}</p>
-  )
-}
+  return <p>{props.part} {props.exercise}</p>;
+};
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3} </p>
-  )
-}
+    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+  );
+};
 
 const App = () => {
   const course = {
-  name: 'Half Stack application development',
-  parts: [{
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: "Using props to pass data",
-    exercises: 7
-  },
-  {
-    name: "State of a component",
-    exercises: 14
-  }]}
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
@@ -40,9 +37,9 @@ const App = () => {
       <Content part={course.parts[0].name} exercise={course.parts[0].exercises} />
       <Content part={course.parts[1].name} exercise={course.parts[1].exercises} />
       <Content part={course.parts[2].name} exercise={course.parts[2].exercises} />
-      <Total ex1={course.parts[0].exercises} ex2={course.parts[1].exercises} ex3={part[2].exercises} />
+      <Total ex1={course.parts[0].exercises} ex2={course.parts[1].exercises} ex3={course.parts[2].exercises} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
