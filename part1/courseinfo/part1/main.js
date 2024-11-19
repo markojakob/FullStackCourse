84fb7753f87951124948a5523a3b57455129d54e
GertenP <gerten.pilv@tptlive.ee>
2024-11-19 12:54:42 +0200

    ex 1.5

---
+++ part1/courseinfo/part1/main.js
@@ -0,0 +1,5 @@
+let t = [1, 2, 3]
+let t2 = []
+t.map(element => {console.log("aleks")})
+
+t.forEach(element => {console.log(element)});
--- part1/courseinfo/part1/src/App.jsx
+++ part1/courseinfo/part1/src/App.jsx
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props)
   return (
     <h1>{props.course}</h1>
   )
@@ -18,21 +19,28 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+  name: 'Half Stack application development',
+  parts: [{
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: "Using props to pass data",
+    exercises: 7
+  },
+  {
+    name: "State of a component",
+    exercises: 14
+  }]}
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+      <Content part={course.parts[0].name} exercise={course.parts[0].exercises} />
+      <Content part={course.parts[1].name} exercise={course.parts[1].exercises} />
+      <Content part={course.parts[2].name} exercise={course.parts[2].exercises} />
+      <Total ex1={course.parts[0].exercises} ex2={course.parts[1].exercises} ex3={part[2].exercises} />
     </div>
   )
 }
