e954a3ef82a110a52610968575f400dbd78838ca
Mark Jakob Pontus <markjakobo@gmail.com>
2024-11-26 12:39:19 +0200

    Deconstruct test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-const Hello = (props) => {
+ /*
+ const Hello = (props) => {
   return (
     <div>
       <p>Hello {props.name}, you are {props.age} years old.</p>
@@ -25,5 +26,35 @@ const App = () => {
     </div>
   )
 }
+*/
 
-export default App
+const Hello = (props) => {
+  const {name, age } = props
+
+  const bornYear = () => new Date().getFullYear() - age
+  
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = "Mark"
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Mark" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App 
