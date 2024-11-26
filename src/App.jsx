59dd1eab447ba2816ce9c883f6cd08d0780d599f
Mark Jakob Pontus <markjakobo@gmail.com>
2024-11-26 14:41:52 +0200

    Statistics

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-  const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+  let All = good + neutral + bad;
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      <h2>
+        give feedpack
+      </h2>
+      <button onClick={() => { setGood(good + 1) }} good>
+        good
+      </button>
+      <button onClick={() => { setNeutral(neutral + 1) }} neutral>
+        neutral
+      </button>
+      <button onClick={() => { setBad(bad + 1) }} bad>
+        bad
+      </button>
+      <h2>
+        statistics
+      </h2>
+      <table>
+        <tr>
+          <td>Good</td>
+          <td>{good}</td>
+        </tr>
+        <tr>
+          <td>Neutral</td>
+          <td>{neutral}</td>
+        </tr>
+        <tr>
+          <td>Bad</td>
+          <td>{bad}</td>
+        </tr>
+        <tr>
+          <td>All</td>
+          <td>{All}</td>
+        </tr>
+        <tr>
+          <td>Average</td>
+          <td>{(good * 1 + neutral * 0 + bad * -1)/All }</td>
+        </tr>
+        <tr>
+          <td>Positive</td>
+          <td>{good/All * 100}%</td>
+        </tr>
+
+      </table>
     </div>
   )
 }
 
-export default App 
+export default App
